96b62520c8978d84dffd47b7d05220ec98e5ea27
Jiayao Yu <yjybxx2@yahoo.com>
2025-02-06 17:00:51 +0100

    Added plant growing script and UI demo. Tested on headset.

--- Assets/Scripts/PlantGrowing.cs
+++ Assets/Scripts/PlantGrowing.cs
@@ -1,18 +1,29 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlantGrowing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<GameObject> _plantsInEachGrowingStage;
+    [SerializeField] private List<GameObject> _vfxInEachGrowingStage;
+
+    [SerializeField] private float _maxInactiveDuration = 2f; // the longest time a user is inactive before the plant withers
+
+    private int _currentStageIndex = 0;
+
+    public void Initialize()
     {
-        
+        // hide all plants for later object pooling
+        foreach (var plant in _plantsInEachGrowingStage)
+        {
+            plant.SetActive(false);
+        }
+        _currentStageIndex = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AdvanceToNextStage()
     {
-        
+        if (_currentStageIndex > 0) _plantsInEachGrowingStage[_currentStageIndex - 1].SetActive(false);
+        _plantsInEachGrowingStage[_currentStageIndex].SetActive(true);
+        if (_currentStageIndex < _plantsInEachGrowingStage.Count) _currentStageIndex++;
     }
 }
