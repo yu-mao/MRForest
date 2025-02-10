f46899abf10ca5f1615fe7f8841dea39a3ae83e0
ranajollanar <ranajollanarbh@gmail.com>
2025-02-10 18:11:22 +0100

    Added particle effects to collectables

--- Assets/Scripts/AddCollectable.cs
+++ Assets/Scripts/AddCollectable.cs
@@ -1,17 +1,44 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AddCollectable : MonoBehaviour
 {
     [SerializeField] private AudioSource collectSound;
+    [SerializeField] private GameObject sunParticlesEffect;
+    [SerializeField] private GameObject waterParticlesEffect;
+    private float particleDuration = 5f; // Duration before deactivating
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Water") || other.CompareTag("Sun"))
+        if (other.CompareTag("Water"))
         {
             other.gameObject.SetActive(false);
             collectSound.Play();
+            ActivateParticles(waterParticlesEffect);
+        }
+        else if (other.CompareTag("Sun"))
+        {
+            other.gameObject.SetActive(false);
+            collectSound.Play();
+            ActivateParticles(sunParticlesEffect);
+        }
+    }
+
+    private void ActivateParticles(GameObject particleEffect)
+    {
+        if (particleEffect != null)
+        {
+            particleEffect.SetActive(true);
+            StartCoroutine(DeactivateAfterDelay(particleEffect, particleDuration));
+        }
+    }
+
+    private IEnumerator DeactivateAfterDelay(GameObject particleEffect, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        if (particleEffect != null)
+        {
+            particleEffect.SetActive(false);
         }
     }
 }
